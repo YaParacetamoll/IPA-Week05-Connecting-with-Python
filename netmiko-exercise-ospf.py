7df3516ffb815d275c1d5f1ef3468a95b821ff12
Pat worada <65070203@kmitl.ac.th>
2024-09-21 14:00:37 +0700

    modifyenvvariable

--- netmiko-exercise-ospf.py
+++ netmiko-exercise-ospf.py
@@ -1,9 +1,11 @@
 from netmiko import ConnectHandler
-from getpass import getpass
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # กำหนด username และ password
-username = "admin"
-password = "cisco"
+username = os.environ.get("USERNAME")
+password = os.environ.get("PASSWORD")
 
 # รายการ IP ของอุปกรณ์
 device_ip = ["172.31.105.3", "172.31.105.4"]
